fd4d8f598b95cc835ccdaf4ac686aead704abf46
MaximCosta <maxim.costa@epitech.eu>
2022-03-01 03:48:42 +0100

    push

--- rplugin/python/nvim-example-python-plugin.py
+++ rplugin/python/nvim-example-python-plugin.py
@@ -7,9 +7,21 @@ class NeotagsPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
+    def echo(self, *msgs):
+        msg = ' '.join([str(m) for m in msgs])
+        self._vim.out_write(msg + '\n')
+
+    def echo_error(self, *msgs):
+        msg = ' '.join([str(m) for m in msgs])
+        self._vim.err_write(msg + '\n')
+
     @neovim.autocmd('BufWritePost', pattern='*', eval='expand("<afile>:p")')
     def update_tags_for_file(self, filename):
         self.nvim.out_write('neotags > ' + message + "\n")
+
+    @neovim.autocmd('BufWritePost', pattern='*')
+    def echo_test(self):
+        self.echo("je suis lambda")
 
     @neovim.function('DoItPython')
     def doItPython(self, args):
